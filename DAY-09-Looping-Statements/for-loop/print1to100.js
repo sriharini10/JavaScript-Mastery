fbd1b4b1076613a967cf94b7865d16c7b4a33218
sriharini10 <sriharini21102004@gmail.com>
2026-07-02 00:03:18 +0530

    loops added

---
+++ DAY-09-Looping-Statements/for-loop/print1to100.js
@@ -0,0 +1,25 @@
+for(let i=1;i<=100;i++){
+    console.log(i);
+}
+
+console.log("-----------reverse-------");
+
+for(let i=50;i>=1;i--){
+    console.log(i)
+}
+
+console.log("----------odd--------------");
+
+for(let i=1;i<=100;i++){
+    if(i%2!=0){
+        console.log(i)
+    }
+}
+
+console.log("----------even--------------");
+
+for(let i=1;i<=100;i++){
+    if(i%2===0){
+        console.log(i)
+    }
+}
---
+++ DAY-09-Looping-Statements/for-loop/sumOfNumbers.js
@@ -0,0 +1,10 @@
+function sumOfNumbers(n) {
+
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        sum=sum+i;
+        
+    }
+     console.log(sum)
+}
+sumOfNumbers(50)
